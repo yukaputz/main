fb9c4213329af6fcd9147b9b221d8dd37c1e191b
yukaputz <yukaputz@gmail.com>
2023-12-18 10:31:35 -0500

    test

--- scratch.ps1
+++ scratch.ps1
@@ -1,6 +1,25 @@
+# Install File Server Resource Manager feature
 Install-WindowsFeature -Name FS-Resource-Manager -IncludeManagementTools
-Set-FsrmSetting -SmtpServer "smtp.example.com" -AdminEmailAddress "admin@example.com" -FromEmailAddress "fsrm@example.com" -EmailSubjectPrefix "[FSRM]"$threshold = New-FsrmQuotaThreshold -Path "C:\path\to\directory" -Percentage 80
-Send-MailMessage -To "recipient@example.com" -From "sender@example.com" -Subject "Test Email" -Body "This is a test email." -SmtpServer "smtp.example.com"
-Set-FsrmSetting -EmailNotificationLimit 1 -EventNotificationLimit 1 -CommandNotificationLimit 1 -ReportNotificationLimit 1
-New-FsrmQuota -Path "C:\path\to\directory" -Size 1GB -Notification $threshold -Template "Monitoring Template"
-New-FsrmFileScreen 
+
+# Configuration variables
+$fileGroup = "AudioVideoFiles"
+$fileScreenTemplateName = "BlockAudioVideoFiles"
+$fileScreenPath = "C:\Shares\BlockingPath"  # Set your desired path
+$notificationTemplateName = "BlockNotificationTemplate"
+$notificationSender = "noreply@example.com"
+$notificationRecipients = "admin@example.com"
+
+# Create file group for audio and video files
+New-FsrmFileGroup -Name $fileGroup -IncludePattern @("*.mp3", "*.wav", "*.mp4", "*.avi")
+
+# Create file screen template for blocking
+New-FsrmFileScreenTemplate -Name $fileScreenTemplateName -IncludeGroup $fileGroup -Action Block
+
+# Create file screen on the specified path using the template
+New-FsrmFileScreen -Path $fileScreenPath -Template $fileScreenTemplateName
+
+# Create notification template
+New-FsrmFileScreenTemplate -Name $notificationTemplateName -IncludeGroup $fileGroup -Action Email -MailTo $notificationRecipients -MailFrom $notificationSender -Subject "Blocked File Notification" -Message "A user attempted to save a blocked file."
+
+# Apply notification template to the file screen
+Set-FsrmFileScreen -Path $fileScreenPath -Notification $notificationTemplateName
